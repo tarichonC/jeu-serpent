ebdcbe6cb1d30d5703eeeec570132d7aa92812c8
tarichonC <32991257+tarichonC@users.noreply.github.com>
2017-10-31 01:33:40 +0100

    Gestion collision
    - Gestion du "mangeage" de pomme
    - Gestion des collision entre serpent/mur et serpent/serpent

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 /* TODO :
     Gérer les collisions :
-        - mur
-        - corps du serpent
+        [*] mur
+        [*] corps du serpent
+        [ ] gérer le game over
 
     Gérer le "mangeage" de pomme :
-        - gérer l'apparition et la disparition d'une pomme.
-        - Agrandir le corps du serpent (ajouter un [x, y]) à la fin du tableau snake.body
-        - Compter des points
-        - Accélerer le serpent au bout d'un certain nb de pommes (réduire le delay)
-        - Pommes pourries ?
+        [*] gérer l'apparition
+        [*] Agrandir le corps du serpent (ajouter un [x, y]) à la fin du tableau snake.body
+        [ ] Compter des points
+        [ ] Accélerer le serpent au bout d'un certain nb de pommes (réduire le delay)
+        [ ] Pommes pourries ?
     
     what else ?
 */
@@ -37,7 +38,9 @@ window.onload = function() {
     var canvas; // L'objet HTML canvas dans lequel on dessine
     var ctx;    // Le contexte du canvas
     var snakee; // Notre serpent  
-    var apple;
+    var apple2eat;
+    var widthInBlock = CANVASW / BLOCKSIZE;
+    var heightInBlock = CANVASH / BLOCKSIZE;
 
     init();
 
@@ -50,22 +53,27 @@ window.onload = function() {
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         snakee = new Snake([[6,4], [5,4], [4,4]]);
-        apple = new Apple([10,10]);
+        apple2eat = new Apple([10,10]);
         refresh();
 
     }
 
     function refresh() {
 
-        xCoord += 2;
-        yCoord += 2;
-        ctx.fillStyle = SNAKE_COLOR;
-        this.direction = KeyEnum.KEY_LEFT;
-        ctx.clearRect(0,0, canvas.width, canvas.height);
-        snakee.draw();
-        apple.draw();
         snakee.move();
-        setTimeout(refresh, delay);  // Ajout du timer
+        if(snakee.collision()) {
+            // GAME OVER
+        }
+        else {
+            ctx.clearRect(0,0, canvas.width, canvas.height);
+            snakee.draw();
+            if(snakee.eat(apple2eat)) {
+                apple2eat.newPosition();
+                snakee.grow();
+            }
+            apple2eat.draw();
+            setTimeout(refresh, delay);  // Ajout du timer
+        }
 
     }
 
@@ -77,6 +85,7 @@ window.onload = function() {
         
         this.body = body;                               // corps du serpent
         this.direction = KeyEnum.KEY_RIGHT;             // direction du serpent
+
         this.draw = function() {
             
             ctx.save();
@@ -146,8 +155,54 @@ window.onload = function() {
 
         this.collision = function() {
             
-            var collision = false;
+            var head = this.body[0];
+            var body = this.body.slice(1);
+            var minX = 0;
+            var minY = 0;
+            var maxX = widthInBlock - 1;
+            var maxY = heightInBlock - 1;
+            var horinzontalCollision = head[0] < minX || head[0] > maxX;
+            var verticalCollision = head[1] < minY || head[1] > maxY;
+
+            if(horinzontalCollision || verticalCollision) return true;
+
+            for(var i=0 ; i<body.length ; i++) {
+                if(head[0] === body[i][0] && head[1] === body[i][1])
+                    return true;
+            }
+
+            return false;
         };
+
+        this.eat = function(apple) {
+            var head = this.body[0] ;
+            if(head[0] === apple2eat.position[0] && head[1] === apple2eat.position[1]) {
+                return true;
+            }
+            else return false;
+        };
+
+        this.grow = function() {
+            var newBlock = this.body[0];
+            switch(this.direction) {
+                case KeyEnum.KEY_DOWN:
+                    ++ newBlock[1];
+                    this.body.unshift(newBlock)
+                    break;
+                case KeyEnum.KEY_UP:
+                    -- newBlock[1];
+                    this.body.unshift(newBlock)
+                    break;
+                case KeyEnum.KEY_LEFT:
+                    -- newBlock[0];
+                    this.body.unshift(newBlock)
+                    break;
+                case KeyEnum.KEY_RIGHT:
+                    ++ newBlock[0];
+                    this.body.unshift(newBlock)
+                    break;
+            }
+        }
         
     }
 
@@ -158,8 +213,8 @@ window.onload = function() {
         this.draw = function() {
 
             var radius = BLOCKSIZE / 2;
-            var x = position[0] * BLOCKSIZE + radius;   
-            var y = position[1] * BLOCKSIZE + radius; 
+            var x = this.position[0] * BLOCKSIZE + radius;   
+            var y = this.position[1] * BLOCKSIZE + radius; 
 
             ctx.save();
             ctx.fillStyle = APPLE_COLOR;
@@ -168,6 +223,11 @@ window.onload = function() {
             ctx.fill();
             ctx.restore();
             
+        };
+
+        this.newPosition = function() {
+            this.position[0] = Math.round(Math.random() * (widthInBlock - 1));
+            this.position[1] = Math.round(Math.random() * (heightInBlock - 1));
         };
     }
 
